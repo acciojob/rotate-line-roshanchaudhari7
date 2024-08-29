ed24022a6c282545023b10a9c4cdf8a7a92e150a
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2024-08-29 22:36:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function rotateLine() {
+    let lineOne = document.getElementById('line');
+    let angle = 0;
+
+    function rotate() {
+        angle = (angle + 2) % 360; 
+        lineOne.style.transform = `rotate(${angle}deg)`;
+        requestAnimationFrame(rotate); 
+    }
+    rotate();
+}
+
+window.onload = rotateLine;
+
